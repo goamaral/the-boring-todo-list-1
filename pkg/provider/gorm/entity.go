4841d2bb55af88d68ab3296c6079f5eb9348dec0
Gonçalo Amaral <goa.docs@gmail.com>
2023-04-05 23:24:38 +0100

    Test TaskFilter and move Id setting to BeforeCreate hook

--- pkg/provider/gorm/entity.go
+++ pkg/provider/gorm/entity.go
@@ -1,9 +1,19 @@
 package gormprovider
 
-import "time"
+import (
+	"time"
+
+	"github.com/oklog/ulid/v2"
+	"gorm.io/gorm"
+)
 
 type AbstractEntity struct {
 	Id        string    `json:"id" gorm:"primarykey"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+func (ae *AbstractEntity) BeforeCreate(tx *gorm.DB) error {
+	ae.Id = ulid.Make().String()
+	return nil
 }
